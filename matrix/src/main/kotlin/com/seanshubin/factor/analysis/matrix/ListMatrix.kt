1d55124789ffe69a8b621980b136e7eccff397a1
Sean Shubin <sshubin@cj.com>
2021-12-15 14:44:34 -0800

    code cleanup

--- matrix/src/main/kotlin/com/seanshubin/factor/analysis/matrix/ListMatrix.kt
+++ matrix/src/main/kotlin/com/seanshubin/factor/analysis/matrix/ListMatrix.kt
@@ -1,57 +1,18 @@
 package com.seanshubin.factor.analysis.matrix
 
-import com.seanshubin.factor.analysis.format.RowStyleTableFormatter
-
 class ListMatrix(private val rows: List<List<Double>>) : Matrix {
+    companion object {
+        val empty: Matrix = ListMatrix(emptyList())
+    }
+
     init {
         requireAllRowsSameSize()
     }
 
     private fun constructor() = ListMatrix(emptyList())
-    private fun constructor(rows: List<List<Double>>) = ListMatrix(rows)
     private fun constructor(cells: List<Double>, columnCount: Int) =
         if (cells.isEmpty() && columnCount == 0) constructor()
         else ListMatrix(cells.chunked(columnCount))
-
-    override fun get(i: Int, j: Int): Double = rows[i][j]
-
-    override fun addRow(vararg cells: Double): Matrix =
-        ListMatrix(rows + listOf(cells.toList()))
-
-    override fun addColumn(vararg cells: Double): Matrix {
-        val baseRows = rows.ifEmpty {
-            (1..cells.size).map { emptyList() }
-        }
-        return ListMatrix(baseRows.zip(cells.toList()).map(::appendCell))
-    }
-
-    private fun appendCell(rowAndCell: Pair<List<Double>, Double>): List<Double> =
-        appendCell(rowAndCell.first, rowAndCell.second)
-
-    private fun appendCell(row: List<Double>, cell: Double): List<Double> = row + cell
-
-    override fun plus(that: Matrix): Matrix = binaryOperation(that) { a, b -> a + b }
-
-    override fun times(that: Matrix): Matrix {
-        TODO("not implemented")
-    }
-
-    override fun binaryOperation(that: Matrix, operation: (Double, Double) -> Double): Matrix {
-        val cells: List<Double> = toList().zip(that.toList()).map { (a, b) -> operation(a, b) }
-        val result = constructor(cells, columnCount)
-        return result
-    }
-
-    override val order: Order get() = Order(rowCount, columnCount)
-
-    override val rowCount: Int get() = rows.size
-
-    override val columnCount: Int get() = if (rowCount == 0) 0 else rows[0].size
-
-    override fun toList(): List<Double> = rows.flatten()
-    override fun toRows(): List<List<Double>> {
-        return rows
-    }
 
     private fun requireAllRowsSameSize() {
         if (rows.isEmpty()) return
@@ -61,11 +22,28 @@ class ListMatrix(private val rows: List<List<Double>>) : Matrix {
         if (!allRowsSameSize) throw RuntimeException("All rows are required to be the same size:\n$this")
     }
 
-    companion object {
-        val empty: Matrix = ListMatrix(emptyList())
+    override val rowCount: Int get() = rows.size
+    override val columnCount: Int get() = if (rowCount == 0) 0 else rows[0].size
+    override fun get(i: Int, j: Int): Double = rows[i][j]
+    override fun addRow(vararg cells: Double): Matrix = ListMatrix(rows + listOf(cells.toList()))
+
+    override fun addColumn(vararg cells: Double): Matrix {
+        fun appendCell(row: List<Double>, cell: Double): List<Double> = row + cell
+        fun appendCell(rowAndCell: Pair<List<Double>, Double>): List<Double> =
+            appendCell(rowAndCell.first, rowAndCell.second)
+
+        val baseRows = rows.ifEmpty {
+            (1..cells.size).map { emptyList() }
+        }
+        return ListMatrix(baseRows.zip(cells.toList()).map(::appendCell))
     }
 
-    override fun toLines(): List<String> = RowStyleTableFormatter.minimal.format(rows)
+    override fun binaryOperation(that: Matrix, operation: (Double, Double) -> Double): Matrix {
+        val cells: List<Double> = toList().zip(that.toList()).map { (a, b) -> operation(a, b) }
+        val result = constructor(cells, columnCount)
+        return result
+    }
+
     override fun toString(): String = toLines().joinToString("\n")
     override fun equals(other: Any?): Boolean =
         when (other) {
@@ -74,4 +52,5 @@ class ListMatrix(private val rows: List<List<Double>>) : Matrix {
         }
 
     override fun hashCode(): Int = rows.hashCode()
+
 }
--- matrix/src/main/kotlin/com/seanshubin/factor/analysis/matrix/Matrix.kt
+++ matrix/src/main/kotlin/com/seanshubin/factor/analysis/matrix/Matrix.kt
@@ -1,22 +1,38 @@
 package com.seanshubin.factor.analysis.matrix
 
+import com.seanshubin.factor.analysis.format.RowStyleTableFormatter
+
 interface Matrix {
     val rowCount: Int
     val columnCount: Int
     operator fun get(i: Int, j: Int): Double
-    fun binaryOperation(that: Matrix, operation: (Double, Double) -> Double): Matrix
     fun addRow(vararg cells: Double): Matrix
     fun addColumn(vararg cells: Double): Matrix
-    fun toList(): List<Double>
-    fun toRows(): List<List<Double>>
+    fun binaryOperation(that: Matrix, operation: (Double, Double) -> Double): Matrix
 
+    fun toList(): List<Double> {
+        val list = mutableListOf<Double>()
+        (0 until rowCount).forEach { rowIndex ->
+            (0 until columnCount).forEach{ columnIndex ->
+                list.add(this[rowIndex, columnIndex])
+            }
+        }
+        return list
+    }
+    fun toRows(): List<List<Double>> {
+        val list = mutableListOf<List<Double>>()
+        (0 until rowCount).forEach { rowIndex ->
+            val row = mutableListOf<Double>()
+            (0 until columnCount).forEach{ columnIndex ->
+                row.add(this[rowIndex, columnIndex])
+            }
+            list.add(row)
+        }
+        return list
+
+    }
+    fun toLines(): List<String> = RowStyleTableFormatter.minimal.format(toRows())
     fun addRow(vararg cells: Int): Matrix = addRow(*cells.map { it.toDouble() }.toDoubleArray())
     fun addColumn(vararg cells: Int): Matrix = addColumn(*cells.map { it.toDouble() }.toDoubleArray())
-
-    operator fun plus(that: Matrix): Matrix
-    operator fun times(that: Matrix): Matrix
-
-    val order: Order get() = Order(rowCount, columnCount)
-
-    fun toLines(): List<String>
+    operator fun plus(that: Matrix): Matrix = binaryOperation(that) { a, b -> a + b }
 }
